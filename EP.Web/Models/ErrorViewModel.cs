520fc4e178d2a5f61e33f48fecaf440dda5f03bd
Mahdi <freelancerdtms@gmail.com>
2022-10-08 17:30:37 +0330

    Add project files.

---
+++ EP.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EP.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
