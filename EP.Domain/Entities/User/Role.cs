e098d00cf3284d4fadaf71934ea164ecaf43ab2e
Mahdi <freelancerdtms@gmail.com>
2022-10-12 22:08:40 +0200

    adding entities part 1 (role , user user is not finished yet)

---
+++ EP.Domain/Entities/User/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP.Domain.Entities.User
+{
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+
+        [Display(Name = "اسم رول")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200,ErrorMessage = "{0} نمیتواند بیشتر از {1} کاراکتر باشد.")]
+        public string RoleName { get; set; }
+    }
+}
---
+++ EP.Domain/Entities/User/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP.Domain.Entities.User
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Display(Name = "")]
+        public string UserName { get; set; }
+    }
+}
